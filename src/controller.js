92a035179c0299ac7a08c68330f5f6e4fb9819e1
AyayronSu <aarondsujana@gmail.com>
2025-12-29 16:21:37 -0600

    Completed main functionality

--- src/controller.js
+++ src/controller.js
@@ -1,105 +1,134 @@
-import { Task } from "./task.js";
-import { Project } from "./project.js";
+    import { Task } from "./task.js";
+    import { Project } from "./project.js";
 
-function initApp() {
-    const newProjectBtn = document.getElementById('new-project');
-    const homeBtn = document.getElementById('home');
-    const projects = [];
-    
-    const defaultProject = new Project('Project One');
-    const defaultTask = new Task('Clean dishes', 'Head to the kitchen and wash dishes before dinner', 'Tonight', 'High');
+    function initApp() {
+        const newProjectBtn = document.getElementById('new-project');
+        const homeBtn = document.getElementById('home');
+        const projects = [];
+        
+        const defaultProject = new Project('Project One');
+        const defaultTask = new Task('Clean dishes', 'Head to the kitchen and wash dishes before dinner', 'Tonight', 'High');
 
-    projects.push(defaultProject);
-    
-    defaultProject.addTask(defaultTask);
-    
-    let activeProject = defaultProject;
+        projects.push(defaultProject);
+        
+        defaultProject.addTask(defaultTask);
+        
+        let activeProject = defaultProject;
 
-    function renderProjects() {
-        const sidebar = document.getElementById('project-list');
-        sidebar.innerHTML = '';
-        projects.forEach(project => {
-            const projectItem = document.createElement('li');
-            projectItem.textContent = project.name;
-            projectItem.classList.add('project-item');
-            projectItem.addEventListener('click', () => {
-                activeProject = project;
+        function renderProjects() {
+            const sidebar = document.getElementById('project-list');
+            sidebar.innerHTML = '';
+            projects.forEach(project => {
+                const projectItem = document.createElement('li');
+                projectItem.textContent = project.name;
+                projectItem.classList.add('project-item');
+                projectItem.addEventListener('click', () => {
+                    activeProject = project;
+                    renderTasks();
+                    renderProjects();
+                })
+
+                if (project === activeProject) {
+                    projectItem.classList.add('active-project');
+                }
+
+                sidebar.appendChild(projectItem);
+            });
+        }
+        
+        function renderTasks() {
+            const taskContainer = document.getElementById('task-container');
+            const projectNameDisplay = document.getElementById('current-project-name');
+        
+            taskContainer.innerHTML = '';
+        
+            projectNameDisplay.textContent = activeProject.name;
+        
+            activeProject.getTasks().forEach(task => {
+                const taskElement = document.createElement('div');
+                taskElement.classList.add('task-card');
+                taskElement.textContent = `${task.title} - ${task.dueDate}`;
+                taskElement.dataset.id = task.id;
+
+                const deleteTaskBtn = document.createElement('button');
+                deleteTaskBtn.classList.add('delete-btn');
+                deleteTaskBtn.textContent = 'Delete';
+
+                taskElement.appendChild(deleteTaskBtn);
+
+                taskContainer.appendChild(taskElement);
+            });
+        }
+
+        
+        function addTasks() {
+            const addTaskBtn = document.getElementById('add-task-btn');
+            const dialog = document.getElementById('task-modal');
+        
+            addTaskBtn.addEventListener('click', () => {
+                dialog.showModal();
+            })
+            
+            const submitTaskBtn = document.getElementById('submit-task');
+            
+            submitTaskBtn.addEventListener('click', (e) => {
+                e.preventDefault();
+                const newTaskTitle = document.getElementById('task-title').value;
+                const newTaskDesc = document.getElementById('task-desc').value;
+                const newTaskDueDate = document.getElementById('task-date').value;
+                const newTaskPriority = document.getElementById('task-priority').value;
+        
+                const newTask = new Task(newTaskTitle, newTaskDesc, newTaskDueDate, newTaskPriority);
+                activeProject.addTask(newTask);
+        
+                document.getElementById('task-form').reset();
+                dialog.close();
+        
                 renderTasks();
-                renderProjects();
+            })
+        
+        }
+
+        function handleTaskInteractions() {
+            const taskContainer = document.getElementById('task-container');
+            taskContainer.addEventListener('click', (e) => {
+                if (e.target.classList.contains('delete-btn')) {
+                    const taskId = e.target.parentElement.dataset.id;
+                    activeProject.removeTaskById(taskId);
+                    renderTasks();
+                }
+            })
+        }
+
+        function addProjects() {
+            const addProjectBtn = document.getElementById('new-project');
+            const dialog = document.getElementById('project-modal');
+
+            addProjectBtn.addEventListener('click', () => {
+                dialog.showModal();
             })
 
-            if (project === activeProject) {
-                projectItem.classList.add('active-project');
-            }
+            const submitProjectBtn = document.getElementById('submit-project');
 
-            sidebar.appendChild(projectItem);
-        });
-    }
-    
-    function renderTasks() {
-        const taskContainer = document.getElementById('task-container');
-        const projectNameDisplay = document.getElementById('current-project-name');
-    
-        taskContainer.innerHTML = '';
+            submitProjectBtn.addEventListener('click', (e) => {
+                e.preventDefault();
+                const newProjectTitle = document.getElementById('project-title').value;
 
-        taskContainer.addEventListener('click', (e) => {
-            if (e.target.classList.contains('delete-btn')) {
-                const taskId = e.target.parentElement.dataset.id;
-                activeProject.removeTaskById(taskId);
-                renderTasks();
-            }
-        })
-    
-        projectNameDisplay.textContent = activeProject.name;
-    
-        activeProject.getTasks().forEach(task => {
-            const taskElement = document.createElement('div');
-            taskElement.classList.add('task-card');
-            taskElement.textContent = `${task.title} - ${task.dueDate}`;
-            taskElement.dataset.id = task.id;
+                const newProject = new Project(newProjectTitle);
+                projects.push(newProject);
 
-            const deleteTaskBtn = document.createElement('button');
-            deleteTaskBtn.classList.add('delete-btn');
-            deleteTaskBtn.textContent = 'Delete';
+                document.getElementById('project-form').reset();
+                dialog.close();
 
-            taskElement.appendChild(deleteTaskBtn);
+                renderProjects();
+            })
+        }
 
-            taskContainer.appendChild(taskElement);
-        });
+        renderProjects();
+        renderTasks();
+        addTasks();
+        handleTaskInteractions();
+        addProjects();
     }
 
-    
-    function addTasks() {
-        const addTaskBtn = document.getElementById('add-task-btn');
-        const dialog = document.getElementById('task-modal');
-    
-        addTaskBtn.addEventListener('click', () => {
-            dialog.showModal();
-        })
-        
-        const submitTaskBtn = document.getElementById('submit-task');
-        
-        submitTaskBtn.addEventListener('click', (e) => {
-            e.preventDefault();
-            const newTaskTitle = document.getElementById('task-title').value;
-            const newTaskDesc = document.getElementById('task-desc').value;
-            const newTaskDueDate = document.getElementById('task-date').value;
-            const newTaskPriority = document.getElementById('task-priority').value;
-    
-            const newTask = new Task(newTaskTitle, newTaskDesc, newTaskDueDate, newTaskPriority);
-            activeProject.addTask(newTask);
-    
-            document.getElementById('task-form').reset();
-            dialog.close();
-    
-            renderTasks();
-        })
-    
-    }
-
-    renderProjects();
-    renderTasks();
-    addTasks();
-}
-
-export { initApp }
+    export { initApp }
